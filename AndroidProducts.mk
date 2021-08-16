3703384c9b7131271658966662733c94cb314527
SirRGB <liam_wagner@gmx.de>
2021-08-16 16:17:26 +0200

    change source to fluid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_TP1803.mk
+    $(LOCAL_DIR)/fluid_TP1803.mk
+
+    COMMON_LUNCH_CHOICES := \
+    fluid_TP1803-user \
+    fluid_TP1803-userdebug \
+    fluid_TP1803-eng
--- fluid_TP1803.mk
+++ fluid_TP1803.mk
@@ -6,7 +6,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
 
 # Inherit from TP1803 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -14,7 +14,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Nubia
 PRODUCT_DEVICE := TP1803
 PRODUCT_MANUFACTURER := Nubia
-PRODUCT_NAME := aosp_TP1803
+PRODUCT_NAME := fluid_TP1803
 PRODUCT_MODEL := Mini 5G
 
 PRODUCT_GMS_CLIENTID_BASE := android-nubia
