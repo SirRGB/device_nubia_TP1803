2e960ac0494dc283fbd9b250e5933f03debf86dc
SirRGB <liam_wagner@gmx.de>
2021-08-19 16:59:07 +0200

    add aicp trees for TP1803

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fluid_TP1803.mk
+    $(LOCAL_DIR)/aicp_TP1803.mk
 
     COMMON_LUNCH_CHOICES := \
-    fluid_TP1803-user \
-    fluid_TP1803-userdebug \
-    fluid_TP1803-eng
+    aicp_TP1803-user \
+    aicp_TP1803-userdebug \
+    aicp_TP1803-eng
